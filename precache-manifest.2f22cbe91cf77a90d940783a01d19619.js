b363e0e33731a131636b4c3f329fb35428767686
sdCarr <sarerac@gmail.com>
2025-05-01 20:14:02 +0200

    Deploy

---
+++ precache-manifest.2f22cbe91cf77a90d940783a01d19619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4a1a935ca994cfc7680bd685b0b2f0",
+    "url": "/Said-system-articulation-info/index.html"
+  },
+  {
+    "revision": "7d15b0b7f66bdee89236",
+    "url": "/Said-system-articulation-info/static/css/main.6292832c.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/Said-system-articulation-info/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Said-system-articulation-info/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "7d15b0b7f66bdee89236",
+    "url": "/Said-system-articulation-info/static/js/main.09825472.chunk.js"
+  },
+  {
+    "revision": "e2d47f7cf2c6f67caeaa",
+    "url": "/Said-system-articulation-info/static/js/runtime~main.9b177e35.js"
+  }
+]);
